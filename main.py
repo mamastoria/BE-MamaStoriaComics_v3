d7fe0154d8a8f148c180537d40fa562f456f402c
yantoapri <yapri177@gmail.com>
2026-01-14 20:17:20 +0700

    update cors origin

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "https://mamastoria.com",
-        "http://127.0.0.1:5500",
+        "http://localhost:5500",
         "*"
     ],
     allow_credentials=False,
